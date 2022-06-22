e1085fd7aec6ca761f0e6950d5286c7a2f783bd5
“mohdmyyusuf@gmail.com” <mohdmyyusuf@gmail.com>
2022-06-22 13:54:34 +0530

    Test

--- AssignmentProject_BJS/src/main/java/com/assign/qa/pages/Popuptest.java
+++ AssignmentProject_BJS/src/main/java/com/assign/qa/pages/Popuptest.java
@@ -1,19 +1,50 @@
 package com.assign.qa.pages;
 
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 import com.assign.qa.base.TestBaseAssi;
 
-public class Popuptest extends TestBaseAssi{
-	
-	Popuptest(){
-		
+public class Popuptest extends TestBaseAssi {
+
+	Popuptest() {
+
 		PageFactory.initElements(driver, this);
 	}
 
 	@FindBy(xpath = "")
 	WebElement testName;
+
+	@Test(testName = "verify handles", priority = 1)
+	public void testWindowHandles(){
+		String parent = driver.getWindowHandle();
+	Set<String> child = driver.getWindowHandles();
+	Iterator <String> whs = child.iterator();
+
+	while(whs.hasNext())
+	{
+		String childWin = whs.next();
+		if(!parent.equals(childWin)) {
+			
+			driver.switchTo().window(childWin);
+			
+			
+		}
+	}
+	}
 	
+	@Test(testName = "test popups", priority = 2)
+	public void testPopups() {
+		Alert alert = driver.switchTo().alert();
+		String msg = driver.switchTo().alert().getText();
+		
+		
+	}
+
 }
